cc8e7bce6445e440e1d256adf402f69ae306ddb4
brunomoura <brunocastromoura@gmail.com>
2023-12-01 17:34:57 -0300

    adding cors to the projet

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes';
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
